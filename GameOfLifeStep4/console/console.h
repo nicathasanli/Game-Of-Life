eea4259edefaef055f9046fe802861ed7d0436da
nicathasanli <hesenlinicat2002@gmail.com>
2022-06-12 01:40:04 +0400

    Step4 Added

---
+++ GameOfLifeStep4/console/console.h
@@ -0,0 +1,12 @@
+#ifndef CONSOLE
+#define CONSOLE
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<unistd.h>
+#include"cell.h"
+#include"gameplace.h"
+
+void printInConsole();
+
+#endif
