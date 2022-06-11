736dd1b4f13d3c54cf45575aaeeb923eb163caca
nicathasanli <hesenlinicat2002@gmail.com>
2022-06-11 22:11:26 +0400

    Game Cell

---
+++ gamecell.c
@@ -0,0 +1,21 @@
+#include "gamecell.h"
+Cell newCell(int row, int col)
+{
+    Cell cell;
+    cell.row = row;
+    cell.column = col;
+    cell.isFull = 0;
+    return cell;
+}
+
+void printNeighbours(Cell c)
+{
+    for (int i = -1; i <= 1; i++)
+    {
+        for (int j = -1; j <= 1; j++)
+        {
+            printf("|%d %d|", c.row + i, c.column + j);
+        }
+        printf("\n");
+    }
+}
